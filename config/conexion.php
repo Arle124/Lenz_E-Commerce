ba97274f88f29d6fbbba7ab3ec62618e69a5fc75
ByJaby <javierrico040602@gmail.com>
2025-12-02 11:20:10 -0500

    listop

--- config/conexion.php
+++ config/conexion.php
@@ -1,28 +1,28 @@
 <?php
-class Database {
+    class Database {
 
-    private $host = "localhost";
-    private $dbname = "bd_lenz";
-    private $usuario = "root";
-    private $password = "";
-    private static $conexion = null;
+        private $host = "localhost";
+        private $dbname = "bdlenz";
+        private $usuario = "root";
+        private $password = "";
+        private static $conexion = null;
 
-    public static function conectar() {
-        if (self::$conexion == null) {
+        public static function conectar() {
+            if (self::$conexion == null) {
+                $db = new self(); // crear instancia para acceder a variables no estáticas
+                $dsn = "mysql:host={$db->host};dbname={$db->dbname};charset=utf8mb4";
 
-            $dsn = "mysql:host=localhost;dbname=bd_lenz;charset=utf8mb4";
+                try {
+                    self::$conexion = new PDO($dsn, $db->usuario, $db->password);
+                    self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    self::$conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-            try {
-                self::$conexion = new PDO($dsn, "root", "");
-                self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                self::$conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-            } catch (PDOException $e) {
-                die("Error de conexión: " . $e->getMessage());
+                } catch (PDOException $e) {
+                    die("Error de conexión: " . $e->getMessage());
+                }
             }
-        }
 
-        return self::$conexion;
+            return self::$conexion;
+        }
     }
-}
-?>
+
