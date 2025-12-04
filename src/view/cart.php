10e731a9a51b36fbd75643a2222210b18a920607
Shecid <asheryesid@gmail.com>
2025-12-03 23:40:54 -0500

    'se cargo el archivo sesion.php'

--- src/view/cart.php
+++ src/view/cart.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+require_once __DIR__ . "/../../config/sesion.php"; 
 require_once __DIR__ . '/../../config/autoload.php';
 require_once PATH_CONFIG . "config.php";
 require_once __DIR__ . '/../controller/ProductoController.php';
